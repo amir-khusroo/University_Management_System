e2388d2e45252048a778d1186369da6729ea4cad
amir-khusroo <amirkhusroo250@gmail.com>
2026-01-27 12:48:42 +0530

    Authentication Manager

--- src/main/java/com/UniversityManagementSystem/security/WebSecurityConfig.java
+++ src/main/java/com/UniversityManagementSystem/security/WebSecurityConfig.java
@@ -1,13 +1,22 @@
 package com.UniversityManagementSystem.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final CustomUserDetailService customUserDetailService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -18,6 +27,25 @@ public class WebSecurityConfig {
                 )
                 .formLogin(Customizer.withDefaults());
         return httpSecurity.build();
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity, InMemoryUserDetailsManager inMemoryUserDetailsManager, PasswordEncoder passwordEncoder) throws Exception {
+        AuthenticationManagerBuilder builder= httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+
+        DaoAuthenticationProvider adminProvider=new DaoAuthenticationProvider();
+        adminProvider.setUserDetailsService(inMemoryUserDetailsManager);
+        adminProvider.setPasswordEncoder(passwordEncoder);
+
+        DaoAuthenticationProvider dbProvider=new DaoAuthenticationProvider();
+        dbProvider.setUserDetailsService(customUserDetailService);
+        dbProvider.setPasswordEncoder(passwordEncoder);
+
+        builder.authenticationProvider(adminProvider).authenticationProvider(dbProvider);
+
+        return builder.build();
+
 
     }
 
